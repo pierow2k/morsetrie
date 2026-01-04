183bc71ae5fa66b4ad18c453d3a78b2996ff462c
pierow2k <pierow2k@foo.org>
2026-01-04 06:50:15 -0600

    test(morsetrie): add fuzz tests

---
+++ pkg/morsetrie/fuzz_test.go
@@ -0,0 +1,44 @@
+// Package morsetrie_test provides black-box tests, benchmarks, and runnable
+// examples for the public API of the morsetrie package.
+//
+
+package morsetrie_test
+
+import (
+	"errors"
+	"strings"
+	"testing"
+
+	"github.com/pierow2k/morsetrie/pkg/morsetrie"
+)
+
+// FuzzDecode performs fuzz testing for the Decode method.
+func FuzzDecode(f *testing.F) {
+	f.Add(".... . .-.. .-.. --- / .-- --- .-. .-.. -..")
+	f.Add("-- --- .-. ... . - .-. .. .")
+	f.Add("Abracadabra - Plain text should fail.")
+
+	trie, err := morsetrie.BuildTrie(morsetrie.MorseTable)
+	if err != nil {
+		panic(err)
+	}
+
+	f.Fuzz(func(t *testing.T, input string) {
+		_, err := trie.Decode(input)
+
+		// Check if input contains invalid characters.
+		isValid := true
+		for _, r := range input {
+			if !strings.ContainsRune(".- / \t\n\r", r) {
+				isValid = false
+				break
+			}
+		}
+
+		if isValid && err != nil {
+			t.Errorf("Decode(%q) unexpected error: %v", input, err)
+		} else if !isValid && !errors.Is(err, morsetrie.ErrUnexpectedChar) {
+			t.Errorf("Decode(%q) expected ErrUnexpectedChar, got %v", input, err)
+		}
+	})
+}
