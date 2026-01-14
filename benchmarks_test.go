e1cf9f952062e0cd16cae2f0de6bc52a0f114b61
pierow2k <pierow2k@foo.org>
2026-01-14 06:27:29 -0600

    test: update tests to accommodate static trie

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -8,26 +8,70 @@ import (
 	"github.com/pierow2k/morsetrie"
 )
 
+// input is the morse code of the text:
+// THE QUICK BROWN FOOX JUMPS OVER THE LAZY DOG 0123456789
+const input = "- .... . / --.- ..- .. -.-. -.- / -... .-. --- .-- -. / " +
+	"..-. --- -..-. --- -..- / .--- ..- -- .--. ... / --- ...- . .-. / " +
+	"- .... . / .-.. .- --.. -.-- / -.. --- --. / ----- .---- ..--- " +
+	"...-- ....- ..... -.... --... ---.. ----."
+
+var (
+	trieSink   *morsetrie.Trie
+	decodeSink string
+)
+
 // BenchmarkDecode measures the performance of the Decode function.
+// It does not include the time to build the trie, measuring only
+// the process of decoding.
 func BenchmarkDecode(b *testing.B) {
 	trie, err := morsetrie.BuildTrie(morsetrie.MorseTable)
 	if err != nil {
 		b.Fatal(err)
 	}
-
-	input := "- .... . / --.- ..- .. -.-. -.- / -... .-. --- .-- -. / ..-. " +
-		"--- -..- / .--- ..- -- .--. ... / --- ...- . .-. / - .... . / .-.. " +
-		".- --.. -.-- / -.. --- --. / ----- .---- ..--- ...-- ....- ..... " +
-		"-.... --... ---.. ----."
-
 	for b.Loop() {
-		_, _ = trie.Decode(input)
+		out, _ := trie.Decode(input)
+		decodeSink = out
+	}
+}
+
+// BenchmarkDecode_LoadingTrie measures the performance of the Decode
+// function and includes the process of building the trie using the data
+// from MorseTable.
+func BenchmarkDecode_LoadingTrie(b *testing.B) {
+	for b.Loop() {
+		trie, err := morsetrie.BuildTrie(morsetrie.MorseTable)
+		if err != nil {
+			b.Fatal(err)
+		}
+		out, _ := trie.Decode(input)
+		decodeSink = out
+		trieSink = trie
+	}
+}
+
+// BenchmarkDecode_Using_StaticTrie measures the performance of the Decode
+// function using the static trie.
+func BenchmarkDecode_Using_StaticTrie(b *testing.B) {
+	trie := morsetrie.StaticTrie
+	for b.Loop() {
+		out, _ := trie.Decode(input)
+		decodeSink = out
 	}
 }
 
 // BenchmarkBuildTrie measures the performance of the BuildTrie function.
+// It builds the trie using data from the MorseTable.
 func BenchmarkBuildTrie(b *testing.B) {
 	for b.Loop() {
-		_, _ = morsetrie.BuildTrie(morsetrie.MorseTable)
+		trie, _ := morsetrie.BuildTrie(morsetrie.MorseTable)
+		trieSink = trie
+	}
+}
+
+// BenchmarkLoadStaticTrie measures the performance of loading the static
+// trie.
+func BenchmarkLoadStaticTrie(b *testing.B) {
+	for b.Loop() {
+		trieSink = morsetrie.StaticTrie
 	}
 }
--- morsetrie_test.go
+++ morsetrie_test.go
@@ -11,8 +11,9 @@ import (
 	"github.com/pierow2k/morsetrie"
 )
 
-// TestTrie_Decode provides unit tests for the Decode method.
-func TestTrie_Decode(t *testing.T) {
+// TestTrie_Decode_BuildTrie provides unit tests for the Decode method
+// using the BuildTrie function.
+func TestTrie_Decode_BuildTrie(t *testing.T) {
 	t.Parallel()
 
 	type fields struct {
@@ -128,10 +129,146 @@ func TestTrie_Decode(t *testing.T) {
 		t.Run(testCase.name, func(t *testing.T) {
 			t.Parallel()
 
+			// Build the trie using the pairs.
 			trie, err := morsetrie.BuildTrie(testCase.fields.pairs)
 			if err != nil {
 				t.Fatalf("BuildTrie() error = %v", err)
 			}
+
+			got, err := trie.Decode(testCase.args.input)
+			if (err != nil) != testCase.wantErr {
+				t.Errorf("Trie.Decode() error = %v, wantErr %v", err, testCase.wantErr)
+
+				return
+			}
+
+			if got != testCase.want {
+				t.Errorf("Trie.Decode() = %v, want %v", got, testCase.want)
+			}
+		})
+	}
+}
+
+// TestTrie_Decode_StaticTrie provides unit tests for the Decode method
+// using the static trie.
+func TestTrie_Decode_StaticTrie(t *testing.T) {
+	t.Parallel()
+
+	type fields struct {
+		pairs []morsetrie.MorsePair
+	}
+
+	type args struct {
+		input string
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "basic_sos",
+			fields: fields{
+				pairs: []morsetrie.MorsePair{
+					{Code: "...", R: 'S'},
+					{Code: "---", R: 'O'},
+				},
+			},
+			args: args{
+				input: "... --- ...",
+			},
+			want:    "SOS",
+			wantErr: false,
+		},
+		{
+			name: "standard_hello_world",
+			fields: fields{
+				pairs: morsetrie.MorseTable,
+			},
+			args: args{
+				input: ".... . .-.. .-.. --- / .-- --- .-. .-.. -..",
+			},
+			want:    "HELLO WORLD",
+			wantErr: false,
+		},
+		{
+			name: "resume with accented e",
+			fields: fields{
+				pairs: morsetrie.MorseTable,
+			},
+			args: args{
+				input: ".-. ..-.. ... ..- -- ..-..",
+			},
+			want:    "RÉSUMÉ",
+			wantErr: false,
+		},
+		{
+			name: "punctuation",
+			fields: fields{
+				pairs: morsetrie.MorseTable,
+			},
+			args: args{
+				input: ".-.-.- --..-- ---... ..--.. .----. -....- -..-. " +
+					"-.--. -.--.- .-..-. -...- .-.-. .--.-.",
+			},
+			want:    ".,:?’–/()\"=+@",
+			wantErr: false,
+		},
+		{
+			name: "multiple consecutive word separators",
+			fields: fields{
+				pairs: morsetrie.MorseTable,
+			},
+			args: args{
+				input: "- .... . // --.- ..- .. -.-. -.- / / " +
+					"-... .-. --- .-- -. / ..-. --- -..-",
+			},
+			want:    "THE QUICK BROWN FOX",
+			wantErr: false,
+		},
+		{
+			name: "unknown_sequence",
+			fields: fields{
+				pairs: morsetrie.MorseTable,
+			},
+			args: args{
+				input: "........",
+			},
+			want:    "?",
+			wantErr: false,
+		},
+		{
+			name: "mixed_valid_and_unknown",
+			fields: fields{
+				pairs: morsetrie.MorseTable,
+			},
+			args: args{
+				input: ".... . .-.. .-.. --- / ........ / .-- --- .-. .-.. -..",
+			},
+			want:    "HELLO ? WORLD",
+			wantErr: false,
+		},
+		{
+			name: "invalid_character",
+			fields: fields{
+				pairs: morsetrie.MorseTable,
+			},
+			args: args{
+				input: "abc",
+			},
+			want:    "",
+			wantErr: true,
+		},
+	}
+	for _, testCase := range tests {
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			// Load the StaticTrie instead of building.
+			trie := morsetrie.StaticTrie
 
 			got, err := trie.Decode(testCase.args.input)
 			if (err != nil) != testCase.wantErr {
