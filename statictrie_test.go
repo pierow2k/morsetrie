0ba4e832c22ae3ed7bc7ab89ddfc0afe0c26b9ee
pierow2k <pierow2k@foo.org>
2026-01-16 16:01:33 -0600

    test: add test for full static character set

--- statictrie_test.go
+++ statictrie_test.go
@@ -144,3 +144,24 @@ func TestTrie_Decode_StaticTrie(t *testing.T) {
 		})
 	}
 }
+
+// TestDecodeStatic tests the package-level decode function for each
+// character that is supported by the static trie.
+func TestDecodeStatic(t *testing.T) {
+	t.Parallel()
+
+	morseCode := ".- / -... / -.-. / -.. / . / ..-.. / ..-. / --. / .... / " +
+		".. / .--- / -.- / .-.. / -- / -. / --- / .--. / --.- / .-. / ... / - / " +
+		"..- / ...- / .-- / -..- / -.-- / --.. / ----- / .---- / ..--- / ...-- / " +
+		"....- / ..... / -.... / --... / ---.. / ----. / .-.-.- / --..-- / " +
+		"---... / ..--.. / .----. / -....- / -..-. / -.--. / -.--.- / .-..-. / " +
+		"-...- / .-.-. / .--.-."
+
+	want := "A B C D E É F G H I J K L M N O P Q R S T U V W X Y Z " +
+		"0 1 2 3 4 5 6 7 8 9 . , : ? ’ – / ( ) \" = + @"
+
+	result, _ := morsetrie.DecodeStatic(morseCode)
+	if result != want {
+		t.Errorf("DecodeStatic() = %v, want %v", result, want)
+	}
+}
